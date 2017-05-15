21d835474cb643681c528112b46c9d779ccf1aba
zhaoshisheng <zhaoshisheng@haima.me>
2017-05-15 13:46:01 +0800

    视频功能源码接入；sharesdk无效包删除；部分bug修复；
    Change-Id: Ic0b2812b345188edb669d6b030ad161f3bf91852

--- app/src/main/java/com/tianyu/seelove/ui/activity/system/ShareActivity.java
+++ app/src/main/java/com/tianyu/seelove/ui/activity/system/ShareActivity.java
@@ -10,17 +10,25 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.Toast;
+
+import com.tencent.connect.share.QQShare;
+import com.tencent.connect.share.QzoneShare;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
 import com.tianyu.seelove.R;
 import com.tianyu.seelove.adapter.ShareListAdapter;
-import com.tianyu.seelove.manager.ShareManager;
+import com.tianyu.seelove.common.Constant;
 import com.tianyu.seelove.model.entity.share.ShareInfo;
-import com.tianyu.seelove.model.enums.ShareTo;
 import com.tianyu.seelove.ui.activity.base.BaseActivity;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 分享信息界面
+ *
  * @author shisheng.zhao
  * @date 2017-04-22 17:23
  */
@@ -29,14 +37,14 @@ public class ShareActivity extends BaseActivity implements AdapterView.OnItemCli
     private Button btnCancel;
     private ShareListAdapter adapter;
     private boolean isGridViewItemCanClick = true;// GridView条目是否可点击
-    private ShareManager shareManager;
+    private Tencent mTencent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setCurrentColor("#00000000");
         setContentView(R.layout.activity_share);
-        shareManager = new ShareManager(this);
+        mTencent = Tencent.createInstance(Constant.QQ_APP_ID, this.getApplicationContext());
         initView();
         initData();
     }
@@ -48,10 +56,10 @@ public class ShareActivity extends BaseActivity implements AdapterView.OnItemCli
         btnCancel.setOnClickListener(this);
     }
 
-    private void initData(){
+    private void initData() {
         List<ShareInfo> shareList = new ArrayList<>();
-        shareList.add(new ShareInfo(R.mipmap.share_wechat_icon, getString(R.string.share_wechat)));
-        shareList.add(new ShareInfo(R.mipmap.share_wechatspace_icon, getString(R.string.share_wechat_space)));
+//        shareList.add(new ShareInfo(R.mipmap.share_wechat_icon, getString(R.string.share_wechat)));
+//        shareList.add(new ShareInfo(R.mipmap.share_wechatspace_icon, getString(R.string.share_wechat_space)));
         shareList.add(new ShareInfo(R.mipmap.share_qq_icon, getString(R.string.share_qq)));
         shareList.add(new ShareInfo(R.mipmap.share_qqspace_icon, getString(R.string.share_qq_space)));
 //        shareList.add(new ShareInfo(R.mipmap.share_sinaweibo_icon, getString(R.string.share_sina_weibo_space)));
@@ -72,25 +80,67 @@ public class ShareActivity extends BaseActivity implements AdapterView.OnItemCli
         Intent intent;
         switch (shareInfo.getShareId()) {
             case R.mipmap.share_wechat_icon: // 微信
-                shareManager.share(ShareTo.WECHAT);
                 break;
             case R.mipmap.share_wechatspace_icon: // 朋友圈
-                shareManager.share(ShareTo.WECHAT_CIRCLE);
                 break;
             case R.mipmap.share_qq_icon: // QQ
-                shareManager.share(ShareTo.QQ);
+                onClickShare();
                 break;
             case R.mipmap.share_qqspace_icon: // QQ空间
-                shareManager.share(ShareTo.QQ_CIRCLE);
-                break;
-            case R.mipmap.share_sinaweibo_icon: // 新浪微博
-                shareManager.share(ShareTo.SINA_WEIBO);
+                shareToQzone();
                 break;
             default:
                 break;
         }
         finish();
         overridePendingTransition(R.anim.down_in, R.anim.down_out);
+    }
+
+    private void onClickShare() {
+        final Bundle params = new Bundle();
+        params.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_DEFAULT);
+        params.putString(QQShare.SHARE_TO_QQ_TITLE, "比图文更真实，短视屏相亲，就在视爱App");
+        params.putString(QQShare.SHARE_TO_QQ_SUMMARY, "视爱－让你看见最真实的那个他/她");
+        params.putString(QQShare.SHARE_TO_QQ_TARGET_URL, "http://shiai360.com");
+        params.putString(QQShare.SHARE_TO_QQ_IMAGE_URL, "http://resource.shiai360.com/image/icon.jpg");
+        params.putString(QQShare.SHARE_TO_QQ_APP_NAME, "视爱");
+        params.putInt(QQShare.SHARE_TO_QQ_EXT_INT, 1);
+        mTencent.publishToQzone(ShareActivity.this, params, new BaseUiListener());
+    }
+
+    private void shareToQzone() {
+        final Bundle params = new Bundle();
+        params.putInt(QQShare.SHARE_TO_QQ_KEY_TYPE, QQShare.SHARE_TO_QQ_TYPE_DEFAULT);
+        params.putString(QQShare.SHARE_TO_QQ_TITLE, "比图文更真实，短视屏相亲，就在视爱App");
+        params.putString(QQShare.SHARE_TO_QQ_SUMMARY, "视爱－让你看见最真实的那个他/她");
+        params.putString(QQShare.SHARE_TO_QQ_TARGET_URL, "http://shiai360.com");
+        params.putString(QQShare.SHARE_TO_QQ_IMAGE_URL, "http://resource.shiai360.com/image/icon.jpg");
+        params.putString(QQShare.SHARE_TO_QQ_APP_NAME, "视爱");
+        params.putInt(QQShare.SHARE_TO_QQ_EXT_INT, 1);
+        mTencent.shareToQQ(ShareActivity.this, params, new BaseUiListener());
+    }
+
+    private class BaseUiListener implements IUiListener {
+        @Override
+        public void onComplete(Object response) {
+            //V2.0版本，参数类型由JSONObject 改成了Object,具体类型参考api文档
+            Toast.makeText(ShareActivity.this, "onComplete", Toast.LENGTH_LONG).show();
+            doComplete(response);
+        }
+
+        protected void doComplete(Object values) {
+
+        }
+
+        @Override
+        public void onError(UiError e) {
+            Toast.makeText(ShareActivity.this, "onError", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCancel() {
+            Toast.makeText(ShareActivity.this, "onCancel", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
